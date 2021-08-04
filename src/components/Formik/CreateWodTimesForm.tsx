82cd7c935ddc5fc995e3a6a1d086a8ead800f0ed
Erika Butrimanskiene <erika.butrimanskiene@gmail.com>
2021-08-04 14:38:30 +0300

    fix: create wodtimes form moved to component

---
+++ src/components/Formik/CreateWodTimesForm.tsx
@@ -0,0 +1,236 @@
+import React from 'react';
+import {GestureResponderEvent} from 'react-native';
+import styled, {withTheme, DefaultTheme} from 'styled-components/native';
+import {useTranslation} from 'react-i18next';
+import {useSelector} from 'react-redux';
+import {Formik} from 'formik';
+import {Picker} from '@react-native-picker/picker';
+
+import {createTimeSchema} from '../../utils/formsValidations';
+import {RootState} from '../../state/reducers';
+import {IWorkoutState} from '../../state/workouts/workoutsInterface';
+
+import FormInput from '../../components/FormInput';
+
+interface ICreateWodTimesFormProps {
+  theme: DefaultTheme;
+  setWodsTimes: any;
+  wodsTimes: object[];
+}
+
+const CreateWodTimesForm: React.FC<ICreateWodTimesFormProps> = ({
+  theme,
+  setWodsTimes,
+  wodsTimes,
+}) => {
+  const {t} = useTranslation();
+
+  const wod: IWorkoutState = useSelector(
+    (state: RootState) => state.workouts.selectedWorkout,
+  );
+
+  const handleTimesSet = (
+    wodTime: string,
+    wodRoom: string,
+    coachName: string,
+    attendeesNumber: string,
+  ) => {
+    setWodsTimes([
+      ...wodsTimes,
+      {wodTime, wodRoom, coachName, attendeesNumber},
+    ]);
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        wodTime: '',
+        coachName: '',
+        wodRoom: '',
+        attendeesNumber: '',
+      }}
+      validationSchema={createTimeSchema}
+      onSubmit={(values, {resetForm, setFieldValue}) => {
+        handleTimesSet(
+          values.wodTime,
+          values.wodRoom,
+          values.coachName,
+          values.attendeesNumber,
+        );
+
+        setFieldValue('wodTime', '');
+        setFieldValue('wodRoom', '');
+        setFieldValue('coachName', '');
+        setFieldValue('attendeesNumber', '');
+
+        resetForm();
+      }}>
+      {formikProps => {
+        return (
+          <>
+            <PickerWrapper>
+              <StyledPicker
+                selectedValue={formikProps.values.wodTime}
+                dropdownIconColor={'#ffffff'}
+                onValueChange={itemValue =>
+                  formikProps.setFieldValue('wodTime', itemValue)
+                }>
+                <Picker.Item
+                  style={{fontSize: 20}}
+                  label={t('admin:selectWodTime')}
+                  value={''}
+                  key={0}
+                />
+                <Picker.Item
+                  style={{fontSize: 18}}
+                  label="06:00"
+                  value={'06:00'}
+                  key={1}
+                />
+                <Picker.Item
+                  style={{fontSize: 18}}
+                  label="07:00"
+                  value={'07:00'}
+                  key={2}
+                />
+                <Picker.Item
+                  style={{fontSize: 18}}
+                  label="12:00"
+                  value={'12:00'}
+                  key={3}
+                />
+                <Picker.Item
+                  style={{fontSize: 18}}
+                  label="17:00"
+                  value={'17:00'}
+                  key={3}
+                />
+                <Picker.Item
+                  style={{fontSize: 18}}
+                  label="18:00"
+                  value={'18:00'}
+                  key={3}
+                />
+              </StyledPicker>
+            </PickerWrapper>
+
+            {formikProps.touched.wodTime && formikProps.errors.wodTime && (
+              <ErrorText>{formikProps.errors.wodTime}</ErrorText>
+            )}
+
+            <PickerWrapper>
+              <StyledPicker
+                selectedValue={formikProps.values.wodRoom}
+                dropdownIconColor={'#ffffff'}
+                onValueChange={itemValue =>
+                  formikProps.setFieldValue('wodRoom', itemValue)
+                }>
+                <Picker.Item
+                  style={{fontSize: 20}}
+                  label={t('admin:selectRoom')}
+                  value={''}
+                  key={0}
+                />
+                <Picker.Item
+                  style={{fontSize: 18}}
+                  label="1"
+                  value={'1'}
+                  key={1}
+                />
+                <Picker.Item
+                  style={{fontSize: 18}}
+                  label="2"
+                  value={'2'}
+                  key={2}
+                />
+                <Picker.Item
+                  style={{fontSize: 18}}
+                  label="3"
+                  value={'3'}
+                  key={3}
+                />
+              </StyledPicker>
+            </PickerWrapper>
+
+            {formikProps.touched.wodRoom && formikProps.errors.wodRoom && (
+              <ErrorText>{formikProps.errors.wodRoom}</ErrorText>
+            )}
+
+            <FormInput
+              value={formikProps.values.coachName}
+              placeholderText={t('admin:typeCoachName')}
+              onChangeText={formikProps.handleChange('coachName')}
+              placeholderColor={theme.appColors.textColorLightGray}
+              isInputWithAction={false}
+              bgColor={theme.appColors.backgroundColorLighter}
+            />
+
+            {formikProps.touched.coachName && formikProps.errors.coachName && (
+              <ErrorText>{formikProps.errors.coachName}</ErrorText>
+            )}
+
+            <FormInput
+              value={formikProps.values.attendeesNumber}
+              placeholderText={t('admin:typeAttendeesNumber')}
+              onChangeText={formikProps.handleChange('attendeesNumber')}
+              placeholderColor={theme.appColors.textColorLightGray}
+              isInputWithAction={false}
+              bgColor={theme.appColors.backgroundColorLighter}
+            />
+
+            {formikProps.touched.attendeesNumber &&
+              formikProps.errors.attendeesNumber && (
+                <ErrorText>{formikProps.errors.attendeesNumber}</ErrorText>
+              )}
+
+            <AddButtonContainer
+              onPress={
+                formikProps.handleSubmit as unknown as (
+                  event: GestureResponderEvent,
+                ) => void
+              }>
+              <AddText>{t('admin:addTimeBtn')}</AddText>
+            </AddButtonContainer>
+          </>
+        );
+      }}
+    </Formik>
+  );
+};
+
+const AddButtonContainer = styled.TouchableOpacity`
+  background-color: ${({theme}) => theme.appColors.backgroundColorLighter};
+  margin-top: 15px;
+  align-items: center;
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+const AddText = styled.Text`
+  color: ${({theme}) => theme.appColors.whiteColor};
+  font-size: 20px;
+`;
+
+const ErrorText = styled.Text`
+  color: ${({theme}) => theme.appColors.accentColor};
+  font-size: 17px;
+  padding-bottom: 15px;
+`;
+
+const PickerWrapper = styled.View`
+  margin-top: 10px;
+  margin-bottom: 5px;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  background-color: ${({theme}) => theme.appColors.backgroundColorLighter};
+  border-radius: 10px;
+`;
+
+const StyledPicker = styled(Picker)`
+  height: 45px;
+  color: ${({theme}) => theme.appColors.textColorLightGray};
+  font-size: 30px;
+  width: 100%;
+`;
+export default withTheme(CreateWodTimesForm);
--- src/containers/AdminFlow/CreateWodView.tsx
+++ src/containers/AdminFlow/CreateWodView.tsx
@@ -1,20 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import {
-  TouchableOpacity,
-  ScrollView,
-  GestureResponderEvent,
-  View,
-  Text,
-} from 'react-native';
+import {TouchableOpacity, ScrollView, View, Text} from 'react-native';
 import styled, {withTheme, DefaultTheme} from 'styled-components/native';
 import {useTranslation} from 'react-i18next';
 import {useDispatch, useSelector} from 'react-redux';
 import {StackNavigationProp} from '@react-navigation/stack';
 import DateTimePicker from 'react-native-modal-datetime-picker';
 import {Formik} from 'formik';
-import {Picker} from '@react-native-picker/picker';
 
-import {createWodSchema, createTimeSchema} from '../../utils/formsValidations';
+import {createWodSchema} from '../../utils/formsValidations';
 import ROUTES from '../../routes/Routes';
 import {RootStackParamList} from 'src/routes/Interface';
 import {actions} from '../../state/actions';
@@ -25,6 +18,7 @@ import {IWorkoutState} from '../../state/workouts/workoutsInterface';
 
 import Button from '../../components/Button';
 import FormInput from '../../components/FormInput';
+import CreateWodTimesForm from '../../components/Formik/CreateWodTimesForm';
 
 type CreateWodScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -65,18 +59,6 @@ const CreateWodView: React.FC<ICreateWodViewProps> = ({theme, navigation}) => {
   const handleDatePicked = (date: Date) => {
     console.log('A date has been picked: ', date);
     hideDateTimePicker();
-  };
-
-  const handleDatesSet = (
-    wodTime: string,
-    wodRoom: string,
-    coachName: string,
-    attendeesNumber: string,
-  ) => {
-    setWodsTimes([
-      ...wodsTimes,
-      {wodTime, wodRoom, coachName, attendeesNumber},
-    ]);
   };
 
   const handleCreateFormSubmit = () => {
@@ -162,168 +144,10 @@ const CreateWodView: React.FC<ICreateWodViewProps> = ({theme, navigation}) => {
                   </View>
                 ))}
 
-                <Formik
-                  initialValues={{
-                    wodTime: '',
-                    coachName: '',
-                    wodRoom: '',
-                    attendeesNumber: '',
-                  }}
-                  validationSchema={createTimeSchema}
-                  onSubmit={(values, {resetForm}) => {
-                    handleDatesSet(
-                      values.wodTime,
-                      values.wodRoom,
-                      values.coachName,
-                      values.attendeesNumber,
-                    );
-
-                    formikProps.setFieldValue('wodTime', '');
-                    formikProps.setFieldValue('wodRoom', '');
-                    formikProps.setFieldValue('coachName', '');
-                    formikProps.setFieldValue('attendeesNumber', '');
-
-                    resetForm();
-                  }}>
-                  {formikProps => {
-                    return (
-                      <>
-                        <PickerWrapper>
-                          <StyledPicker
-                            selectedValue={formikProps.values.wodTime}
-                            dropdownIconColor={'#ffffff'}
-                            onValueChange={itemValue =>
-                              formikProps.setFieldValue('wodTime', itemValue)
-                            }>
-                            <Picker.Item
-                              style={{fontSize: 20}}
-                              label={t('admin:selectWodTime')}
-                              value={''}
-                              key={0}
-                            />
-                            <Picker.Item
-                              style={{fontSize: 18}}
-                              label="06:00"
-                              value={'06:00'}
-                              key={1}
-                            />
-                            <Picker.Item
-                              style={{fontSize: 18}}
-                              label="07:00"
-                              value={'07:00'}
-                              key={2}
-                            />
-                            <Picker.Item
-                              style={{fontSize: 18}}
-                              label="12:00"
-                              value={'12:00'}
-                              key={3}
-                            />
-                            <Picker.Item
-                              style={{fontSize: 18}}
-                              label="17:00"
-                              value={'17:00'}
-                              key={3}
-                            />
-                            <Picker.Item
-                              style={{fontSize: 18}}
-                              label="18:00"
-                              value={'18:00'}
-                              key={3}
-                            />
-                          </StyledPicker>
-                        </PickerWrapper>
-
-                        {formikProps.touched.wodTime &&
-                          formikProps.errors.wodTime && (
-                            <ErrorText>{formikProps.errors.wodTime}</ErrorText>
-                          )}
-
-                        <PickerWrapper>
-                          <StyledPicker
-                            selectedValue={formikProps.values.wodRoom}
-                            dropdownIconColor={'#ffffff'}
-                            onValueChange={itemValue =>
-                              formikProps.setFieldValue('wodRoom', itemValue)
-                            }>
-                            <Picker.Item
-                              style={{fontSize: 20}}
-                              label={t('admin:selectRoom')}
-                              value={''}
-                              key={0}
-                            />
-                            <Picker.Item
-                              style={{fontSize: 18}}
-                              label="1"
-                              value={'1'}
-                              key={1}
-                            />
-                            <Picker.Item
-                              style={{fontSize: 18}}
-                              label="2"
-                              value={'2'}
-                              key={2}
-                            />
-                            <Picker.Item
-                              style={{fontSize: 18}}
-                              label="3"
-                              value={'3'}
-                              key={3}
-                            />
-                          </StyledPicker>
-                        </PickerWrapper>
-
-                        {formikProps.touched.wodRoom &&
-                          formikProps.errors.wodRoom && (
-                            <ErrorText>{formikProps.errors.wodRoom}</ErrorText>
-                          )}
-
-                        <FormInput
-                          value={formikProps.values.coachName}
-                          placeholderText={t('admin:typeCoachName')}
-                          onChangeText={formikProps.handleChange('coachName')}
-                          placeholderColor={theme.appColors.textColorLightGray}
-                          isInputWithAction={false}
-                          bgColor={theme.appColors.backgroundColorLighter}
-                        />
-
-                        {formikProps.touched.coachName &&
-                          formikProps.errors.coachName && (
-                            <ErrorText>
-                              {formikProps.errors.coachName}
-                            </ErrorText>
-                          )}
-
-                        <FormInput
-                          value={formikProps.values.attendeesNumber}
-                          placeholderText={t('admin:typeAttendeesNumber')}
-                          onChangeText={formikProps.handleChange(
-                            'attendeesNumber',
-                          )}
-                          placeholderColor={theme.appColors.textColorLightGray}
-                          isInputWithAction={false}
-                          bgColor={theme.appColors.backgroundColorLighter}
-                        />
-
-                        {formikProps.touched.attendeesNumber &&
-                          formikProps.errors.attendeesNumber && (
-                            <ErrorText>
-                              {formikProps.errors.attendeesNumber}
-                            </ErrorText>
-                          )}
-
-                        <AddButtonContainer
-                          onPress={
-                            formikProps.handleSubmit as unknown as (
-                              event: GestureResponderEvent,
-                            ) => void
-                          }>
-                          <AddText>{t('admin:addTimeBtn')}</AddText>
-                        </AddButtonContainer>
-                      </>
-                    );
-                  }}
-                </Formik>
+                <CreateWodTimesForm
+                  wodsTimes={wodsTimes}
+                  setWodsTimes={setWodsTimes}
+                />
 
                 <CreateButtonContainer>
                   <Button
@@ -383,44 +207,10 @@ const CreateButtonContainer = styled.View`
   width: 100%;
 `;
 
-const AddButtonContainer = styled.TouchableOpacity`
-  background-color: ${({theme}) => theme.appColors.backgroundColorLighter};
-  margin-top: 15px;
-  align-items: center;
-  padding: 10px;
-  border-radius: 5px;
-`;
-
-const AddText = styled.Text`
-  color: ${({theme}) => theme.appColors.whiteColor};
-  font-size: 20px;
-`;
-
 const ErrorText = styled.Text`
   color: ${({theme}) => theme.appColors.accentColor};
   font-size: 17px;
   padding-bottom: 15px;
 `;
 
-const PickerWrapper = styled.View`
-  margin-top: 10px;
-  margin-bottom: 5px;
-  justify-content: center;
-  align-items: center;
-  width: 90%;
-  background-color: ${({theme}) => theme.appColors.backgroundColorLighter};
-  border-radius: 10px;
-`;
-
-const StyledPicker = styled(Picker)`
-  height: 45px;
-  color: ${({theme}) => theme.appColors.textColorLightGray};
-  font-size: 30px;
-  width: 100%;
-`;
-
-const FlatListContainer = styled.View`
-  width: 85%;
-  margin-bottom: 180px;
-`;
 export default withTheme(CreateWodView);
