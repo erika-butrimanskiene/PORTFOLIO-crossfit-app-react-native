704edd0136f8e83fc42469af35feca540109ffd3
Erika Butrimanskiene <erika.butrimanskiene@gmail.com>
2021-08-22 00:18:50 +0300

    feat: create functions to count wods per months, thisYear and thisMonth

--- src/utils/getUserFilteredWods.ts
+++ src/utils/getUserFilteredWods.ts
@@ -62,3 +62,82 @@ export const getUserPreviousWods = (userWods: IuserWod[]): IuserWod[] => {
   );
   return filteredPreviousWodsList;
 };
+
+export const getThisYearFromAnyMonthWodsLenght = (
+  userWods: IuserWod[],
+  month: string,
+) => {
+  const previousWods = getUserPreviousWods(userWods);
+  const thisYear = today.getFullYear();
+
+  if (month.length === 1) {
+    month = `0${month}`;
+  }
+  let thisYearOrMonthWods = previousWods.filter(wod => {
+    return wod.wodDate >= `${thisYear}-${month}-01`;
+  });
+  return thisYearOrMonthWods.length;
+};
+
+export const getAnyMonthStartDate = (monthsBefore: number) => {
+  const thisYear = today.getFullYear();
+  const thisMonth = today.getMonth() + 1;
+  let month;
+  let year;
+
+  if (thisMonth + monthsBefore > 0) {
+    year = thisYear;
+    month = `${thisMonth + monthsBefore}`;
+  } else {
+    year = thisYear - 1;
+    month = `${thisMonth + monthsBefore + 12}`;
+  }
+
+  if (month.length === 1) {
+    month = `0${month}`;
+  }
+  return `${year}-${month}-01`;
+};
+
+export const getLastTwelveEveryMonthDateRange = () => {
+  let lastTwelveEveryMonthDateRange = [
+    {start: getAnyMonthStartDate(-11), end: getAnyMonthStartDate(-10)},
+    {start: getAnyMonthStartDate(-10), end: getAnyMonthStartDate(-9)},
+    {start: getAnyMonthStartDate(-9), end: getAnyMonthStartDate(-8)},
+    {start: getAnyMonthStartDate(-8), end: getAnyMonthStartDate(-7)},
+    {start: getAnyMonthStartDate(-7), end: getAnyMonthStartDate(-6)},
+    {start: getAnyMonthStartDate(-6), end: getAnyMonthStartDate(-5)},
+    {start: getAnyMonthStartDate(-5), end: getAnyMonthStartDate(-4)},
+    {start: getAnyMonthStartDate(-4), end: getAnyMonthStartDate(-3)},
+    {start: getAnyMonthStartDate(-3), end: getAnyMonthStartDate(-2)},
+    {start: getAnyMonthStartDate(-2), end: getAnyMonthStartDate(-1)},
+    {start: getAnyMonthStartDate(-1), end: getAnyMonthStartDate(0)},
+    {start: getAnyMonthStartDate(0), end: getAnyMonthStartDate(1)},
+  ];
+  console.log(lastTwelveEveryMonthDateRange);
+  return lastTwelveEveryMonthDateRange;
+};
+
+export const getLastTwelveEveryMonthWodsLenght = (userWods: IuserWod[]) => {
+  let lastTwelveEveryMonthDateRange = getLastTwelveEveryMonthDateRange();
+  let wodsLenghtMonthly: number[] = [];
+  lastTwelveEveryMonthDateRange.forEach(item => {
+    let length = getLengthOfOneAnyMonthWods(userWods, item.start, item.end);
+    wodsLenghtMonthly.push(length);
+  });
+  console.log(wodsLenghtMonthly);
+  return wodsLenghtMonthly;
+};
+
+export const getLengthOfOneAnyMonthWods = (
+  userWods: IuserWod[],
+  startDate: string,
+  endDate: string,
+) => {
+  const previousWods = getUserPreviousWods(userWods);
+  let monthWods = previousWods.filter(wod => {
+    return wod.wodDate >= startDate && wod.wodDate < endDate;
+  });
+
+  return monthWods.length;
+};
